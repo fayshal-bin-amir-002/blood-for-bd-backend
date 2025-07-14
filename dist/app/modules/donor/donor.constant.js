1b20f612c21027242aac085e0152e7093ee5c522
fayshal-bin-amir-002 <foyshalbinamir@gmail.com>
2025-07-15 02:06:27 +0600

    Remove dist folder from repo and add to .gitignore

--- dist/app/modules/donor/donor.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.donorFilterableFields = void 0;
-exports.donorFilterableFields = [
-    "blood_group",
-    "division",
-    "district",
-    "sub_district",
-];
